2593d070b4e7d9e1c0dbf5265ecd5f44ab6ac84f
Mitsiky <90917286+Mitsiky7@users.noreply.github.com>
2021-11-26 14:02:41 +0100

    Initial commit

---
+++ js/ancre.js
@@ -0,0 +1,13 @@
+$(function() {
+    /**
+     * Smooth scrolling to a specific element 
+     **/
+    function scrollTo(target) {
+        if (target.length) {
+            $("html, body").stop().animate({ scrollTop: target.offset().top }, 1500);
+        }
+    }
+
+    // exemple
+    scrollTo($("#a_propos"));
+});
